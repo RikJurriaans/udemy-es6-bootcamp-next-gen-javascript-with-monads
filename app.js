0befdc45bb8ef0be74cb9ed739253c01ba7caeb2
Rik Sanchez <rik@jurriaans.org>
2017-12-01 15:43:04 +0100

    Finished app.

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ import { kleisli } from './kleisli.js';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
-function updateWeatherInterface(jsonData) {
-    console.log(jsonData);
+function updateWeatherInterface(weatherData) {
+    ELEMENTS.ELEMENT_WEATHER_CITY.textContent = weatherData.cityName;
+    ELEMENTS.ELEMENT_WEATHER_DESCRIPTION.textContent = weatherData.description;
+    ELEMENTS.ELEMENT_WEATHER_TEMPERATURE.textContent = weatherData.temperature;
+    ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'block';
+    ELEMENTS.ELEMENT_LOADING_TEXT.style.display = 'none';
 }
 
-function errorOut(errorMessage) {
-    console.log(errorMessage);
+function errorOut(httpStatus) {
+    // Show not found element.
+    console.log("The api responded with something different than 200:" + httpStatus);
 }
 
 const APP_ID = "3ffbb4d7c4c147eeecb26ca4e8b337c0";
 
-let cityNameToAPIUrl = (cityName) => 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&units=metric&appid=" + APP_ID;
+let cityNameToAPIUrl = (cityName) =>
+    'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&units=metric&appid=" + APP_ID;
 
 let weatherJsonToWeatherData = cityName => weatherData => {
     const WEATHER_DATA = new WeatherData(cityName, weatherData.weather[0].description);
@@ -25,17 +31,19 @@ let weatherJsonToWeatherData = cityName => weatherData => {
 };
 
 function searchWeather() {
+    ELEMENTS.ELEMENT_LOADING_TEXT.style.display = 'block';
+
     const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
     const MAYBE_CITY_WEATHER =
-          Maybe.fromFalsy(CITY_NAME)
-               .map(cityNameToAPIUrl)
-               .map((url) =>
-                   kleisli(http.fetchData)
-                      .pipeTo(weatherJsonToWeatherData(CITY_NAME))
-                      .run(url)
-                      .then(updateWeatherInterface)
-                      .catch(errorOut));
+        Maybe
+          .fromFalsy(CITY_NAME)
+          .map(cityNameToAPIUrl)
+          .map((url) =>
+              kleisli(http.fetchData)
+                  .pipeTo(weatherJsonToWeatherData(CITY_NAME))
+                  .run(url)
+                  .then(updateWeatherInterface)
+                  .catch(errorOut));
 
-    // Here you can give an error message displaying the user.
     MAYBE_CITY_WEATHER.orElseRun(() => alert("Field is empty."));
 }
--- http.js
+++ http.js
@@ -5,11 +5,11 @@ export class http {
             const HTTP = new XMLHttpRequest();
             HTTP.open('GET', url);
             HTTP.onreadystatechange = function() {
-                if (HTTP.readyState === XMLHttpRequest.DONE && HTTP.status === 200) {
+                if (HTTP.readyState == XMLHttpRequest.DONE && HTTP.status == 200) {
                     const RESPONSE_DATA = JSON.parse(HTTP.responseText);
                     resolve(RESPONSE_DATA);
-                } else if (HTTP.readyState === XMLHttpRequest.DONE) {
-                    reject('Something went wrong');
+                } else if (HTTP.readyState == XMLHttpRequest.DONE) {
+                    reject(HTTP.status);
                 }
             };
             HTTP.send();
