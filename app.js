193651193196107434bee2aa77639ce006025357
Rik Sanchez <rik@jurriaans.org>
2017-12-01 14:26:36 +0100

    Fixed bussines logic.

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 import * as ELEMENTS from './elements.js';
+import http from './http.js';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
-function searchWeather(cityName) {
+function updateField(jsonData) {
+    console.log(jsonData);
 }
 
-function failtJsonFetch(failureError) {
-    console.log('There has been an error in the api: ' + failure);
+function errorOut(errorMessage) {
+    console.log(errorMessage);
 }
 
 function searchWeather() {
     const MAYBE_CITY_WEATHER =
           Maybe.fromFalsy(ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim())
-               .bind((cityName) => {
-                   console.log('Do search request!');
-                   return Either.Left("Error connecting to api...");
-               })
-               .cata((failure) => {
-                   // here you can log an error message to the log files.
-                   console.log("City not found, error message: " + failure);
-                   return Maybe.none();
-               }, (success) => {
-                   console.log(success);
-                   return Maybe.some(success);
-               });
+               .map((cityName) => 'www.rikkie.nl/' + cityName) // make the url.
+               .map((url) =>
+                        http.fetchData(url)
+                            .then(updateField)
+                            .catch(errorOut));
 
-    // Here you can give an error message displaying the user.
-    MAYBE_CITY_WEATHER.orElseRun(() => alert("There has been an error."));
-}
+        // Here you can give an error message displaying the user.
+        MAYBE_CITY_WEATHER.orElseRun(() => alert("There has been an error."));
+    }
