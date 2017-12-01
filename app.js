2d4492a8a7aa0f9038ea71d65025204c83aaee4b
Rik Sanchez <rik@jurriaans.org>
2017-12-01 15:09:20 +0100

    Refactoring

--- app.js
+++ app.js
@@ -15,23 +15,26 @@ function errorOut(errorMessage) {
 
 const APP_ID = "3ffbb4d7c4c147eeecb26ca4e8b337c0";
 
+let cityNameToAPIUrl = (cityName) => 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&units=metric&appid=" + APP_ID;
+
+let weatherJsonToWeatherData = cityName => weatherData => {
+    const WEATHER_DATA = new WeatherData(cityName, weatherData.weather[0].description);
+    const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
+    WEATHER_PROXY.temperature = weatherData.main.temp;
+    return WEATHER_DATA;
+};
+
 function searchWeather() {
     const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
     const MAYBE_CITY_WEATHER =
           Maybe.fromFalsy(CITY_NAME)
-               .map((cityName) => 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&units=metric&appid=" + APP_ID) // make the url.
-               .map((url) => {
+               .map(cityNameToAPIUrl)
+               .map((url) =>
                    kleisli(http.fetchData)
-                       .pipeTo((weatherData) => {
-                           const WEATHER_DATA = new WeatherData(CITY_NAME, weatherData.weather[0].description);
-                           const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
-                           WEATHER_PROXY.temperature = weatherData.main.temp;
-                           return WEATHER_DATA;
-                       })
-                       .run(url)
-                       .then(updateWeatherInterface)
-                       .catch(errorOut);
-               });
+                      .pipeTo(weatherJsonToWeatherData(CITY_NAME))
+                      .run(url)
+                      .then(updateWeatherInterface)
+                      .catch(errorOut));
 
     // Here you can give an error message displaying the user.
     MAYBE_CITY_WEATHER.orElseRun(() => alert("Field is empty."));
