1043563faa4d81454584a472fd787a7aa89ddd2d
Rik Sanchez <rik@jurriaans.org>
2017-12-01 14:57:15 +0100

    Added api connection. Added kleisli to manipulate data inside the promise

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import * as ELEMENTS from './elements.js';
-import http from './http.js';
+import { http } from './http.js';
+import { WeatherData, WEATHER_PROXY_HANDLER } from './weather-data.js';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
-function updateField(jsonData) {
+class Kleisli {
+    // f :: x -> Monad y
+    constructor(f) {
+        // zo voer je de zooi uit
+        this.run = x => f(x);
+
+        // zo kunnen we kleisli's aan elkaar lijmen
+        this.pipe = g => new Kleisli(x => this.run(x).then(g.run));
+
+        // zo kun je meer dan 1 kleisli aan elkaar lijmen
+        this.pipeTo = g => new Kleisli(x => this.run(x).then(g));
+    };
+}
+
+function updateWeatherInterface(jsonData) {
     console.log(jsonData);
 }
 
@@ -11,15 +26,28 @@ function errorOut(errorMessage) {
     console.log(errorMessage);
 }
 
+let kleisli = (f) => new Kleisli(f);
+
+const APP_ID = "3ffbb4d7c4c147eeecb26ca4e8b337c0";
+
 function searchWeather() {
+    const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
     const MAYBE_CITY_WEATHER =
-          Maybe.fromFalsy(ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim())
-               .map((cityName) => 'www.rikkie.nl/' + cityName) // make the url.
-               .map((url) =>
-                        http.fetchData(url)
-                            .then(updateField)
-                            .catch(errorOut));
+          Maybe.fromFalsy(CITY_NAME)
+               .map((cityName) => 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&units=metric&appid=" + APP_ID) // make the url.
+               .map((url) => {
+                   kleisli(http.fetchData)
+                       .pipeTo((weatherData) => {
+                           const WEATHER_DATA = new WeatherData(CITY_NAME, weatherData.weather[0].description);
+                           const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
+                           WEATHER_PROXY.temperature = weatherData.main.temp;
+                           return WEATHER_DATA;
+                       })
+                       .run(url)
+                       .then(updateWeatherInterface)
+                       .catch(errorOut);
+               });
 
     // Here you can give an error message displaying the user.
-    MAYBE_CITY_WEATHER.orElseRun(() => alert("There has been an error."));
+    MAYBE_CITY_WEATHER.orElseRun(() => alert("Field is empty."));
 }
--- http.js
+++ http.js
@@ -1,21 +1,18 @@
-export default class http {
+export class http {
     // string -> Promise jsObject
     static fetchData(url) {
         return new Promise((resolve, reject) => {
-            setInterval(() => {
-                resolve({weather : '10'});
-            }, 1000);
-            // const HTTP = new XMLHttpRequest();
-            // HTTP.open('GET', url);
-            // HTTP.onreadystatechange = function() {
-            //     if (HTTP.readyState === XMLHttpRequest.DONE && HTTP.status === 200) {
-            //         const RESPONSE_DATA = JSON.parse(HTTP.responseText);
-            //         resolve(RESPONSE_DATA);
-            //     } else if (HTTP.readyState === XMLHttpRequest.DONE) {
-            //         reject('Something went wrong');
-            //     }
-            // };
-            // HTTP.send();
+            const HTTP = new XMLHttpRequest();
+            HTTP.open('GET', url);
+            HTTP.onreadystatechange = function() {
+                if (HTTP.readyState === XMLHttpRequest.DONE && HTTP.status === 200) {
+                    const RESPONSE_DATA = JSON.parse(HTTP.responseText);
+                    resolve(RESPONSE_DATA);
+                } else if (HTTP.readyState === XMLHttpRequest.DONE) {
+                    reject('Something went wrong');
+                }
+            };
+            HTTP.send();
         });
     }
 }
